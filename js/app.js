de067041f8b7661fbed3b91626477d6003940c3c
J Takiz <takizawa.tab@gmail.com>
2020-12-01 23:45:33 +0900

    ページ増設

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
 let validation = new Vue({
   el: '#app',
   data: {
-    name: '',
-    name_kana: '',
+    name_first: '',
+    name_last: '',
+    kana_first: '',
+    kana_last: '',
     email: '',
     email_confirm: '',
     category: '',
     body: '',
     isActive: false,
     errors: {
-      name : [],
-      name_kana : [],
+      name_first : [],
+      name_last : [],
+      kana_first : [],
+      kana_last : [],
       email : [],
       email_confirm : [],
       category : [],
@@ -19,20 +23,37 @@ let validation = new Vue({
   },
   methods: {
     validator: function(type, max) {
-      let name = [];
-      let name_kana = [];
+      let name_first = [];
+      let name_last = [];
+      let kana_first = [];
+      let kana_last = [];
       let email = [];
       let email_confirm = [];
       let category = [];
       let body = [];
       let style = [];
-      let message = max + '文字以内で入力してください。';
+      let message = max + '文字以内';
       
-      if (type === 'name') {
-        if (this.name.length > max) {
-          name.push(message);
+      if (type === 'name_first') {
+        if (this.name_first.length > max) {
+          name_first.push(message);
         }
-        this.errors.name = name;
+        this.errors.name_first = name_first;
+      } else if(type === 'name_last') {
+        if (this.name_last.length > max) {
+          name_last.push(message);
+        }
+        this.errors.name_last = name_last;
+      } else if(type === 'kana_first') {
+        if (this.kana_first.length > max) {
+          kana_first.push(message);
+        }
+        this.errors.kana_first = kana_first;
+      }　else if(type === 'kana_last') {
+        if (this.kana_last.length > max) {
+          kana_last.push(message);
+        }
+        this.errors.kana_last = kana_last;
       }
     }
   }
